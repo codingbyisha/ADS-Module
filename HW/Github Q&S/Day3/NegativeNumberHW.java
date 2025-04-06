4387a5025eb35f19cab9e8721cc00f2952f17f9e
codingbyisha <91665956+codingbyisha@users.noreply.github.com>
2025-04-06 21:02:49 +0530

    Update NegativeNumberHW.java

--- HW/Github Q&S/Day3/NegativeNumberHW.java
+++ HW/Github Q&S/Day3/NegativeNumberHW.java
@@ -1,24 +1,35 @@
-class NegativeNumberHW{
-	public static void main(String args[]){
+//Shift negative lues to front of array
+class RearrangeArrayElements{
+
+    public static void main(String args[]){
         
-		int arr[] = {-12,11,-13,-5,-6,-7,5,-3,-6};
-		
-		int n = 0;
-		int p = arr.length-1;
-		int c=1;
-		int c1=0;
-		while(n<arr.length){
-			if(arr[n]<0){
-				c+=1;
-			}
-			else{
-				c1+=1;
+		int arr[] = {-12,11,-13,-5,6,-7,5,-3,-6};
+		int j=0;
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]<0){
+				if(i!=j){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				j++;
 			}
 		}
 		
-		while(c!=0){
-			
+		int k = arr.length-1;
+		for(int i =j;i< arr.length;i++){
+			if(arr[i]>=0 && i<k){
+				int temp = arr[k];
+				arr[k] = arr[i];
+				arr[i] = temp;
+			}
+			k-=1;
 		}
+		
+		for(int x:arr){
+			System.out.print(" "+x+" ");
+		}
+		
+		
 	}
-	
 }
